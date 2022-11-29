ecbd197e85a63e86604b988175bbb24e7cc01cc8
darlan123df <darlan123df@gmail.com>
2022-11-29 02:38:03 -0300

    Criação da tela de login.

--- src/pages/Logon/index.jsx
+++ src/pages/Logon/index.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 
+import "./styles.css"
+
 export default function Logon() {
     return (
-        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-            <div class="shrink-0">
-                <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
-            </div>
-            <div>
-                <div class="text-xl font-medium text-black">ChitChat</div>
-                <p class="text-slate-500">You have a new message!</p>
-            </div>
+        <div className="container mx-auto w-full max-w-xl m-3 flex justify-center h-screen items-center">
+            <form className="bg-LightGrey shadow-sm rounded px-8 pt-6 pb-8 mb-4 relative" action="">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Usuário:
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Nome de usuário" />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Senha:
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Senha" />
+                    <p className="text-red-500 text-xs italic">Escolha uma senha.</p>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button class="bg-green hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                        Button
+                    </button>
+                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+                        Esqueceu sua senha?
+                    </a>
+                </div>
+            </form>
         </div>
     )
 }
